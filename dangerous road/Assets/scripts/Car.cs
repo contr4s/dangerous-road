7b556233581ab78673c2eaa234027d4697e03189
Vasiliy Yakovlev <yavasya30@mail.ru>
2021-04-27 22:08:07 +0300

    add UI

--- dangerous road/Assets/scripts/Car.cs
+++ dangerous road/Assets/scripts/Car.cs
@@ -1,17 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Car : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class Car: MonoBehaviour
 {
     [SerializeField] private float _motorForce;
     [SerializeField] private WheelCollider[] _wheels;
+    [SerializeField] private UIManager _UIManager;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
 
     private void Start()
     {
-        foreach (var wheel in _wheels)
+        Move(_motorForce);
+    }
+
+    private void Move(float force)
+    {
+        foreach (WheelCollider wheel in _wheels)
         {
-            wheel.motorTorque = _motorForce;
+            wheel.motorTorque = force;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("obstacle"))
+        {
+            _rigidbody.isKinematic = true;
+
+            if (!_UIManager)
+                Debug.LogError("you must assign UIManager on the inspector");
+            else
+                _UIManager.loseDisplay.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("finish"))
+        {
+            _rigidbody.isKinematic = true;
+            if (!_UIManager)
+                Debug.LogError("you must assign UIManager on the inspector");
+            else
+                _UIManager.winDisplay.gameObject.SetActive(true);
         }
     }
 }
--- dangerous road/Assets/scripts/obstacle.cs
+++ dangerous road/Assets/scripts/obstacle.cs
@@ -1,38 +1,49 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.PlayerLoop;
 
+[RequireComponent(typeof(Rigidbody))]
 public class obstacle: MonoBehaviour
 {
-    private bool _selected = false;
+    [SerializeField] private float _swipeTime = 0.1f;
+    [SerializeField] private float _swipeForceScale = 10;
+    [SerializeField] private UIManager _UIManager;
 
+    private bool _hasBeenSwipedAway = false;
+    private Vector3 _cashedPosition;
+
+    private Rigidbody _rigidbody;
     private Camera _mainCam;
 
     private void Awake()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         _mainCam = Camera.main;
     }
 
-    private void OnMouseDown()
+    private void OnMouseEnter()
     {
-        if (!_selected)
-            _selected = true;
-        else
-            _selected = false;
+        StartCoroutine(Swipe(_swipeTime));
     }
 
-    private void Update()
+    private IEnumerator Swipe(float swipeTime)
     {
-        if (_selected)
-            FollowMouse();
+        _cashedPosition = CalulateMousePosition();
+        yield return new WaitForSeconds(swipeTime);
+        Vector3 newPosition = CalulateMousePosition();
+        _rigidbody.velocity = (newPosition - _cashedPosition) * _swipeForceScale;
+        if (!_hasBeenSwipedAway)
+        {
+            if (!_UIManager)
+                Debug.LogError("you must assign UIManager on the inspector");
+            _UIManager.Score++;
+            _hasBeenSwipedAway = true;
+        }
     }
 
-    private void FollowMouse()
+    private Vector3 CalulateMousePosition()
     {
         Vector3 mPos = Input.mousePosition;
-        mPos.z += _mainCam.transform.position.z;
-        transform.position = _mainCam.ScreenToWorldPoint(mPos);
+        mPos.z = Vector3.Distance(_mainCam.transform.position, transform.position);
+        return _mainCam.ScreenToWorldPoint(mPos);
     }
 }
