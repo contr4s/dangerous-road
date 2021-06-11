43d26481620e4a7b5f2f069b41759978311e2588
Vasiliy Yakovlev <yavasya30@mail.ru>
2021-06-11 12:37:28 +0300

    implement 2nd pc input variant

--- dangerous road/Assets/scripts/managers/StandaloneInputManager.cs
+++ dangerous road/Assets/scripts/managers/StandaloneInputManager.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class StandaloneInputManager: InputManager
 {
     private Vector3 _prevTouchPos;
+    private Obstacle _targetObstacle;
 
     void Update()
     {
-        if (CheckIfCanSwipeObstacle(Input.mousePosition, out var obstacle, out float dist))
+        if (Input.GetMouseButton(0))
         {
-            StartCoroutine(Swipe(obstacle, _swipeSO.swipeTime, dist));
-            _prevTouchPos = Input.mousePosition;
+            if (CheckIfCanSwipeObstacle(Input.mousePosition, out var obstacle, out float dist))
+            {
+                //StartCoroutine(Swipe(obstacle, _swipeSO.swipeTime, dist));
+                _prevTouchPos = Input.mousePosition;
+                _targetObstacle = obstacle;
+            }
         }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (_targetObstacle is null)
+                return;
 
+            var direction = (Input.mousePosition - _prevTouchPos);
+            direction.z = 0;
+            direction.Normalize();
+            _targetObstacle.AddForce(swipeForceScale, direction, _targetObstacle.transform.position.z - _mainCam.transform.position.z);
+            StartCoroutine(_targetObstacle.DestroyAfterSwipe(_swipeSO.activeTimeAfterSwipe));
+            _targetObstacle = null;
+        }
     }
 
     public IEnumerator Swipe(Obstacle obstacle, float swipeTime, float distToCam)
