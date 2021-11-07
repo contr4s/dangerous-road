1c0a9ea3bb34fc4f3e1787dd99c7f7441fff2ed7
IBIBENDUM <bah.bah.ru@mail.ru>
2021-11-07 20:59:01 +0300

    Add curved horizon

---
+++ dangerous road/Assets/textures & materials/Bibendum/CaptureScreenshot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace UnityTipsTricks.P00001_Screnshot
+{
+    public class CaptureScreenshot : MonoBehaviour
+    {
+        public int superSize = 2;
+        private int _shotIndex = 0;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                ScreenCapture.CaptureScreenshot($"Screenshot{_shotIndex}.png", superSize);
+                _shotIndex++;
+            }
+        }
+    }
+}
